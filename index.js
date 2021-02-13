56628453e3dcd79a0b87a75429ab1831a3dbda42
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2021-02-13 12:51:19 +0600

    clean up

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 client.commands = new Discord.Collection()
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./commands/${file}`)
 
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
+	client.commands.set(command.name, command)
 }
 
 const prefix = 'y!'
@@ -23,8 +21,6 @@ client.once('ready', ()=> {
 
 
 client.on('message', message => {
-    // console.log(message.content)
-
     if (!message.content.startsWith(prefix) || message.author.bot) {
         return;
     }
@@ -47,7 +43,6 @@ client.on('message', message => {
     }
 
 })
-
 
 
 client.login(process.env.TOKEN)
